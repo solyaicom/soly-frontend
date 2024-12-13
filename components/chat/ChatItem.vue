a30804ca301b5faf0e709d5d9c6ff454a7a9bdac
m1776 <m1776@solyai.com>
2024-12-13 15:14:07 +0700

    mardown text

--- components/chat/ChatItem.vue
+++ components/chat/ChatItem.vue
@@ -1,18 +1,36 @@
 <script setup lang="ts">
 import { IChatMessage } from "~/services/api/chat/type";
+import markdownit from "markdown-it";
+const md = markdownit("commonmark", {
+  html: true,
+  breaks: true,
+  linkify: true,
+});
 
-defineProps<{
+const contentRef = ref<any>(null);
+
+const props = defineProps<{
   item: IChatMessage;
 }>();
+
+watch(
+  [() => props.item.content, () => contentRef.value],
+  () => {
+    if (!props.item.content || !contentRef.value) return;
+    const txt = md.render(props.item.content);
+    contentRef.value.innerHTML = txt;
+  },
+  {
+    immediate: true,
+  }
+);
 </script>
 
 <template>
   <div class="w-full flex flex-col mt-8 px-3" :class="{ 'items-end ': item.role === 'user' }">
     <div class="flex flex-row" :class="{ 'py-3 px-4 rounded-[45px] border-[1px] border-[#FFFFFF1A]': item.role === 'user' }">
       <div v-if="item.role === 'assistant'" class="w-[32px] h-[32px] bg-[#FFFFFFCC] mr-2 rounded-full"></div>
-      <div class="text-[#979797] text-[16px] mt-[2px] flex-1" :class="{ 'text-[#CACACA] mt-0': item.role === 'user' }">
-        {{ item.content }}
-      </div>
+      <div ref="contentRef" class="text-[#979797] text-[16px] mt-[2px] flex-1" :class="{ 'text-[#CACACA] mt-0': item.role === 'user' }"></div>
     </div>
   </div>
 </template>
