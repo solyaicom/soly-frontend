e5346bdd6ba77a568a347c9cf4e648ab405041a0
m1776 <m1776@solyai.com>
2025-01-02 17:21:40 +0700

    fix: ui header home page

--- components/home/Header.vue
+++ components/home/Header.vue
@@ -2,6 +2,7 @@
 import { APP_DATA_LINK } from "~/constants";
 
 const authStore = useAuthStore();
+const { balance } = useSolana();
 
 const menu = ref([
   // { label: "Github", icon: "", url: APP_DATA_LINK.github },
@@ -13,7 +14,7 @@ const user = computed(() => authStore.user);
 </script>
 
 <template>
-  <header class="relative min-h-16 lg:min-h-20 container px-4 sm:px-6 flex items-center">
+  <header class="relative min-h-20 container px-4 sm:px-6 flex items-center">
     <img src="/home/logo.png" alt="logo" class="h-7 w-auto shrink-0" />
     <ul class="flex-1 flex items-center justify-end gap-3 sm:gap-10">
       <li v-for="item in menu" :key="item.label">
@@ -27,10 +28,13 @@ const user = computed(() => authStore.user);
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink v-if="!user.id" to="/auth/login" class="btn__solid btn__solid-white">Log in</NuxtLink>
-        <NuxtLink v-else to="/c" class="border border-white/10 rounded-full btn__solid gap-1.5">
-          <img v-if="user.avatar_url" :src="user.avatar_url" alt="avt" class="w-6 h-6 rounded-full object-cover" />
-          <span>{{ user.name }}</span>
+        <NuxtLink v-if="!user.id" to="/auth/login" class="btn__solid btn__solid-white h-12">Log in</NuxtLink>
+        <NuxtLink v-else to="/c" class="border border-white/10 rounded-full btn__solid gap-2 h-12 pl-2 pr-4">
+          <img v-if="user.avatar_url" :src="user.avatar_url" alt="avt" class="w-8 h-8 rounded-full object-cover" />
+          <div class="text-left">
+            <p class="text-sm">{{ user.wallet.address ? shortAddress(user.wallet.address) : user.name }}</p>
+            <p class="text-xs text-home-neutral-400">{{ formatNumber(balance, 3) }} SOL</p>
+          </div>
         </NuxtLink>
       </li>
     </ul>
