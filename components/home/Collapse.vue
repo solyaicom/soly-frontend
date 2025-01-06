250175a2bbdc380158469fa088bfdf6a594b011c
m1776 <m1776@solyai.com>
2025-01-06 15:57:01 +0700

    save change

--- components/home/Collapse.vue
+++ components/home/Collapse.vue
@@ -12,7 +12,7 @@ defineEmits(["toggle"]);
   <div class="py-6 flex flex-col border-b border-white/10">
     <div class="flex items-center gap-3 cursor-pointer" @click="$emit('toggle')">
       <p class="flex-1 text-white text-base sm:text-xl font-medium">{{ title }}</p>
-      <NuxtIcon name="arrow-up" class="text-xl text-white shrink-0 transition-all" :class="show ? 'rotate-180' : ''" />
+      <NuxtIcon name="arrow-up" class="text-xl text-white shrink-0 transition-all" :class="show ? '' : 'rotate-180'" />
     </div>
     <div v-if="show" class="mt-3 flex flex-col gap-2">
       <template v-if="!content.length">
