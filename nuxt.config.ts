28a2cf71bb74c2d15ccf8de26523a4e29faa9670
m1776 <m1776@solyai.com>
2025-01-09 12:46:23 +0700

    splash screen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,12 +43,13 @@ export default defineNuxtConfig({
     head: {
       style: [{ children: "body { margin: 0 }" }],
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
-
+      title: "Soly AI",
       script: [],
     },
   },
 
   ssr: false,
+  spaLoadingTemplate: "splash-loading.html",
 
   googleSignIn: {
     clientId: "320448324963-luc2s6c99ksmhh3viirt2tfcpkjth6jh.apps.googleusercontent.com",
