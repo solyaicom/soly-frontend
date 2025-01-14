39446a1de9889f9fd61aa2d2cdaffb1a1296f827
m1776 <m1776@solyai.com>
2025-01-14 09:43:22 +0700

    feat: add gtag | release patch

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,7 +44,22 @@ export default defineNuxtConfig({
       style: [{ children: "body { margin: 0 }" }],
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.png" }],
       title: "Soly AI",
-      script: [],
+      script: [
+        {
+          src: "https://www.googletagmanager.com/gtag/js?id=G-3PY9BMF9B5",
+          async: true,
+        },
+        {
+          innerHTML: `
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-3PY9BMF9B5');
+`,
+        },
+      ],
     },
   },
 
