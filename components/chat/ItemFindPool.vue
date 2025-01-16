dc6b3c6905bedb93373ae9893e3255f0e35706b9
m1776 <m1776@solyai.com>
2025-01-16 18:24:11 +0700

    feat: sort

--- components/chat/ItemFindPool.vue
+++ components/chat/ItemFindPool.vue
@@ -8,6 +8,8 @@ const data = convertToolOutput(props.output);
 const pairs = ref<any[]>([]);
 const objToken = ref<{ [key: string]: IFullToken }>({});
 const conversationStore = useConversationStore();
+const isDesc = ref(true);
+const currentType = ref<"liq" | "vol" | "fee">("fee");
 
 onMounted(async () => {
   const ps = ((data.items as { pairs: any[] }[]) || [])
@@ -28,6 +30,25 @@ onMounted(async () => {
   pairs.value = ps;
 });
 
+function onSortClick(type: "liq" | "vol" | "fee") {
+  if (type === "fee") {
+    pairs.value = pairs.value.sort((a, b) => (a.apr < b.apr ? 1 : -1));
+  }
+  if (type === "liq") {
+    pairs.value = pairs.value.sort((a, b) => (a.liquidity < b.liquidity ? 1 : -1));
+  }
+  if (type === "vol") {
+    pairs.value = pairs.value.sort((a, b) => (a.trade_volume_24h < b.trade_volume_24h ? 1 : -1));
+  }
+  if (type === currentType.value) {
+    isDesc.value = !isDesc.value;
+  }
+  if (!isDesc.value) {
+    pairs.value = pairs.value.reverse();
+  }
+  currentType.value = type;
+}
+
 function onViewDetail(address: string) {
   conversationStore.setCurrentMessage(`view detail pool ${address}`);
 }
@@ -36,13 +57,19 @@ function onViewDetail(address: string) {
 <template>
   <div class="rounded-b-[6px]">
     <div class="max-h-[390px] overflow-scroll relative">
-      <table class="w-full min-w-[450px] bg-app-card1 border-separate py-3 px-2 text-center rounded-[6px]">
+      <table class="w-full min-w-[500px] bg-app-card1 border-separate py-3 px-2 text-center rounded-[6px]">
         <thead class="sticky z-[1] top-0 bg-app-card1">
           <tr class="text-app-text2 text-start">
             <td>Pool</td>
-            <td>Liquidity</td>
-            <td>24h Vol</td>
-            <td>24h Fee/TVL</td>
+            <td>
+              <div class="row-center" @click="onSortClick('liq')">Liquidity <NuxtIcon name="icon-sort" /></div>
+            </td>
+            <td>
+              <div class="row-center" @click="onSortClick('vol')">24h Vol <NuxtIcon name="icon-sort" /></div>
+            </td>
+            <td>
+              <div class="row-center" @click="onSortClick('fee')">24h Fee/TVL <NuxtIcon name="icon-sort" /></div>
+            </td>
             <td></td>
           </tr>
         </thead>
