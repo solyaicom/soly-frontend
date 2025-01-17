f19a140dc39bf30e7db1062dd2641574bd1c205d
m1776 <m1776@solyai.com>
2025-01-17 14:54:58 +0700

    feat: add more partner

--- components/home/Partners.vue
+++ components/home/Partners.vue
@@ -15,13 +15,28 @@ const items = ref([
     url: "https://www.assisterr.ai/",
     logoSize: "h-[25px]",
   },
+  {
+    logo: "/images/home/item-logo-llm.png",
+    url: " https://x.com/ai160kg",
+    logoSize: "h-[42px]",
+  },
+  {
+    logo: "/images/home/item-logo-orbit.png",
+    url: "https://www.orbitcryptoai.com/",
+    logoSize: "h-[42px]",
+  },
+  {
+    logo: "/images/home/item-logo-sphere.png",
+    url: "https://sphereone.xyz/",
+    logoSize: "h-[42px]",
+  },
 ]);
 </script>
 
 <template>
   <section id="ads" class="home-container home__gap-section">
     <h2 class="home__title">Notable Partners</h2>
-    <div class="mt-6 sm:mt-10 lg:mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 sm:gap-6">
+    <div class="mt-6 sm:mt-10 lg:mt-14 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
       <a v-for="(p, idx) in items" :key="idx" :href="p.url" target="_blank" class="flex items-center justify-center">
         <img :src="p.logo" :class="p.logoSize" />
       </a>
